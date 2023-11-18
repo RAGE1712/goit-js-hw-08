3366076f540527baab16dc04b833bf8732747e37
RAGE1712 <137414196+RAGE1712@users.noreply.github.com>
2023-11-18 02:15:39 +0200

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+list.insertAdjacentHTML('beforeend', galleryMarkup(galleryItems));
+
+function galleryMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+    </a>
+    </li>
+    `
+    )
+    .join('');
+}
+
+    new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionPosition: 'bottom',
+      captionDelay: 250,
+    });
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector("iframe");
+const player = new VimeoPlayer(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+
+const timeSave = function (data) {
+  let seconds = data.seconds;
+  const key = 'videoplayer-current-time';
+  const value = seconds;
+  saveToLS(key, value);
+};
+
+player.on('timeupdate', timeSave);
